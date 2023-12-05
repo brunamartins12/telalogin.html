9586f06d7956a3ef5ccc8a29e63ea0091b626454
brunamartins12 <134522899+brunamartins12@users.noreply.github.com>
2023-12-05 13:27:34 -0300

    ok

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
-document.getElementById('loginform').addEventListener('submit',function(event)) {
+document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const username = document.getElementById
-}
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+    console.log('User login:', username, password);
+});
+
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('registerUsername').value;
+    const password = document.getElementById('registerPassword').value;
+    const confirmPassword = document.getElementById('registerConfirmPassword').value;
+    if (password !== confirmPassword) {
+        alert('Passwords do not match.');
+        return;
+    }
+    console.log('User register:', username, password);
+});
